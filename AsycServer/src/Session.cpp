7822cf6d300ee66ef40b7c61f1660b0cc96414cd
AhDaii <1171469399@qq.com>
2023-07-09 22:10:20 +0800

    add pseudo-closure

--- AsycServer/src/Session.cpp
+++ AsycServer/src/Session.cpp
@@ -10,28 +10,28 @@ using namespace std;
 void Session::Start() {
     memset(_data, 0, max_length);
     _socket.async_read_some(boost::asio::buffer(_data, max_length),
-                            bind(&Session::handle_read, this, std::placeholders::_1, std::placeholders::_2));
+                            bind(&Session::handle_read, this, std::placeholders::_1, std::placeholders::_2, shared_from_this()));
 }
 
-void Session::handle_read(const boost::system::error_code &ec, size_t bytes_transferred) {
+void Session::handle_read(const boost::system::error_code &ec, size_t bytes_transferred, std::shared_ptr<Session> _self_shared) {
     if (!ec) {
         cout << "Server receive data is: " << _data << endl;
         boost::asio::async_write(_socket, boost::asio::buffer(_data, bytes_transferred),
-                                 std::bind(&Session::handle_write, this, std::placeholders::_1));
+                                 std::bind(&Session::handle_write, this, std::placeholders::_1, _self_shared));
     } else {
         cout << "Read error" << endl;
-        delete this;
+        _server->ClearSession(_uuid);
     }
 }
 
-void Session::handle_write(const boost::system::error_code &ec) {
+void Session::handle_write(const boost::system::error_code &ec, std::shared_ptr<Session> _self_shared) {
     if (!ec) {
         memset(_data, 0, max_length);
         _socket.async_read_some(boost::asio::buffer(_data, max_length),
-                                bind(&Session::handle_read, this, std::placeholders::_1, std::placeholders::_2));
+                                bind(&Session::handle_read, this, std::placeholders::_1, std::placeholders::_2, _self_shared));
     } else {
         cout << "Write error" << endl;
-        delete this;
+        _server->ClearSession(_uuid);
     }
 }
 
@@ -41,15 +41,25 @@ Server::Server(boost::asio::io_context& ioc, unsigned short port):_ioc(ioc), _ac
 }
 
 void Server::start_accept() {
-    Session* new_session = new Session(_ioc);
+//    Session* new_session = new Session(_ioc);
+     shared_ptr<Session> new_session =  make_shared<Session>(_ioc, this);
     _acceptor.async_accept(new_session->Socket(), std::bind(&Server::handle_accept, this, new_session, std::placeholders::_1));
 }
 
-void Server::handle_accept(Session* new_session, const boost::system::error_code& ec) {
+void Server::handle_accept(shared_ptr<Session> new_session, const boost::system::error_code& ec) {
     if (!ec) {
         new_session->Start();
+        _sessions.insert(make_pair(new_session->GetUuid(), new_session));
     } else {
-        delete new_session;
+//        delete new_session;
     }
     start_accept();
+}
+
+std::string& Session::GetUuid() {
+    return _uuid;
+}
+
+void Server::ClearSession(std::string uuid) {
+    _sessions.erase(uuid);
 }
