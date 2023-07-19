2d638b779135174e26beb396bebd358dc30c1b2d
AhDaii <1171469399@qq.com>
2023-07-19 22:40:54 +0800

    use JsonCPP to transmit data

---
+++ AsycServer_JsonCPP/include/const.h
@@ -0,0 +1,12 @@
+//
+// Created by hunz1 on 2023/7/12.
+//
+
+#ifndef ASYCSERVER_CONST_H
+#define ASYCSERVER_CONST_H
+
+#define HEAD_LENGTH 2
+#define MAX_LENGTH  (1024*2)
+#define MAX_SENDQUE 1000
+
+#endif //ASYCSERVER_CONST_H
---
+++ AsycServer_JsonCPP/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <boost/asio.hpp>
+#include <CServer.h>
+
+int main() {
+    try {
+        boost::asio::io_context ioc;
+        using namespace std;
+        CServer s(ioc, 10086);
+        ioc.run();
+    } catch (std::exception& e) {
+        std::cerr << "Exception: " << e.what() << std::endl;
+    }
+    return 0;
+}
